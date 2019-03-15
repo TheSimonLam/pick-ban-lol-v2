7044fa6e1dd451747249edf254a6dc1663676672
Simon Lam <simon.lam@moneysupermarket.com>
2019-03-15 16:41:03 +0000

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
+import { ChampComponent } from './champ/champ.component';
+import { QueueComponent } from './queue/queue.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent,
+    ChampComponent,
+    QueueComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/classes/champ.ts
@@ -0,0 +1,4 @@
+export class Champ {
+  id: number;
+  name: string;
+}
